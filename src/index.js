d43f4b670635c06d9e28cfe343e3ed13157089a5
unknown <pihenriquez.dwfs19@bootcampudd.cl>
2025-09-13 17:05:19 -0300

    Inicialización del proyecto To-Do List con Express y configuración básica

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// Cargar variables de entorno
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const connectDB = require("./config/db");
+
+
+const userRouter = require("./routes/user");
+const taskRouter = require("./routes/task");
+
+// Inicializar la app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Conectar a la base de datos
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());//permite recibir datos en formato JSON
+
+// Rutas
+app.get("/", (req, res) => {
+    res.json({ message: "API To-Do List funcionando!" });
+});
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tasks", taskRouter);
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
